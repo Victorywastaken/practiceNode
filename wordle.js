e46f1e22c652d236b81a4b140d0914a2d86816a5
Victorywastaken <rlatmdfl92@gmail.com>
2022-02-21 21:04:47 -0500

    thinking of basic game mechanics

--- wordle.js
+++ wordle.js
@@ -1,4 +1,7 @@
 const readline = require('readline');
+const os = require('os');
+const fs = require('fs');
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
@@ -17,3 +20,23 @@ rl.on('close', () => {
   console.log('Have a great day!');
   process.exit(0);
 });
+
+class Wordle {
+  constructor(secretWord){
+    this.remainingGuesses = 5;
+    this.gameState = 'playing';
+    this.secretWord = secretWord;
+    this.guessedLetters = [];
+  }
+
+  guessWord(str){
+    if (str === this.secretWord) {
+      this.gameState = 'won';
+    } else {
+      this.remainingGuesses--;
+      if (this.remainingGuesses === 0) {
+        this.gameState = 'lost';
+      }
+    }
+  }
+}
